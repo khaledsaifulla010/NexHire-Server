28f8a3d8e7bc2a06572cf3eef867cbfff5f5f7b0
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-15 20:36:45 +0600

    added cors origin

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const port = process.env.PORT || 5000;
 
 // Middlewares //
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
